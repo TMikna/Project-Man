ca60c1298faf0c8f15cce973982d5af24649ea54
Tomas Mikna <TMikna@users.noreply.github.com>
2018-09-12 15:31:30 +0300

    Revert "Revert "Merge branch 'master' into Tomas""
    This reverts commit 79d8decbc3d20a2fe09679c54e7379611cbaa0c2.

--- ProjectMan/src/projectman/ProjectMan.java
+++ ProjectMan/src/projectman/ProjectMan.java
@@ -9,11 +9,19 @@ import javafx.stage.Stage;
 public class ProjectMan extends Application {
 
     @Override
+<<<<<<< HEAD
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/ui/LoginWindow.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+=======
+    public void start(Stage loginWindowStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
+        loginWindowStage.setTitle("Login");
+        loginWindowStage.setScene(new Scene(root));
+        loginWindowStage.show();
+>>>>>>> master
     }
 
 
